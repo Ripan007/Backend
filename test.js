43a88c6af4a67d68183e749fcf83bee66b3bba72
ripan <reponmondal007@gmail.com>
2023-06-02 14:50:45 +0000

    Jun 2, 2023, 8:20 PM

--- test.js
+++ test.js
@@ -1,13 +1,1 @@
-const cart = [];
 
-export const addCart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(
-    `the cart have the product is ${product} and  quantity is ${quantity}`
-  );
-};
-
-const price = 777;
-const funPrice = 999;
-
-export { price, funPrice };
