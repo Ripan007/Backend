07111c455a56a2018f1a012afc288ca1b5185486
ripan <reponmondal007@gmail.com>
2023-06-02 15:06:40 +0000

    Jun 2, 2023, 8:36 PM

--- index.js
+++ index.js
@@ -127,3 +127,7 @@
 /* -------------------------------------------------------------------------- */
 /*                                   module                                   */
 /* -------------------------------------------------------------------------- */
+
+const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+const data = await res.json();
+console.log(data);
