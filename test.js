b11cc176676ff646d912db72c40009c99ade98f3
ripan <reponmondal007@gmail.com>
2023-06-02 14:38:51 +0000

    Jun 2, 2023, 8:08 PM

--- test.js
+++ test.js
@@ -2,4 +2,9 @@ console.log("export module");
 const shoppingCart = 10;
 const cart = [];
 
-export const addToCart = function () {};
+export const addToCart = function (product, quantity) {
+  cart.push({ quantity, product });
+  console.log(
+    `product is ${product} and quantity is ${quantity} added to the cart`
+  );
+};
