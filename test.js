bde8dec43db10582e36df080c7c2b63fe88cd3b3
ripan <reponmondal007@gmail.com>
2023-06-02 14:59:51 +0000

    Jun 2, 2023, 8:29 PM

--- test.js
+++ test.js
@@ -1,8 +1,2 @@
-console.log("export");
-const shoppingCart = 10;
-export const cart = [];
 
-export default function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`the cart contains  ${product} of ${quantity} quantity`);
-}
+
