c560233c900785411642e924c79b4fdfbd86f247
ripan <reponmondal007@gmail.com>
2023-06-03 03:53:23 +0000

    Jun 3, 2023, 9:23 AM

--- index.js
+++ index.js
@@ -128,6 +128,6 @@
 /*                                   module                                   */
 /* -------------------------------------------------------------------------- */
 
-const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-const data = await res.json();
-console.log(data);
+const shoppingCart = function () {
+  const cart = [];
+};
