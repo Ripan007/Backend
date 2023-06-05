523e6a2bebb7d448a5b388fa5cd2d50c82a09069
ripan <reponmondal007@gmail.com>
2023-06-05 16:48:22 +0000

    Jun 5, 2023, 10:18 PM

--- index.js
+++ index.js
@@ -144,6 +144,7 @@
 
 import inquirer from "inquirer";
 import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([{ message: "type in your URL: ", name: "URL" }])
@@ -151,7 +152,7 @@ inquirer
     // console.log(answers);
     const url = answers.URL;
     var qr_svg = qr.image(url);
-    qr_svg.pipe(require("fs").createWriteStream("qr_img.png"));
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
   })
   .catch((error) => {
     if (error.isTtyError) {
