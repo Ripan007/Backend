50b36fe5c4ec2595a433b13bb88f37140057762c
ripan <reponmondal007@gmail.com>
2023-06-06 05:20:07 +0000

    Jun 6, 2023, 10:50 AM

--- Node/index.js
+++ Node/index.js
@@ -1,7 +1,11 @@
+// creation  of server , routing
 const http = require("http");
 const server = http.createServer((req, res) => {
   //   res.end("welcome to the home page");
   //   console.log(req.url);
+  if (req.url === "/") {
+    res.end(" <h1>home page <h1/>  ");
+  }
 });
 server.listen(1000, () => {
   console.log("server is working");
