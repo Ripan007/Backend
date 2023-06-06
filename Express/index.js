d1b360cec13089a84a3b1604b5b976a4c079497e
ripan <reponmondal007@gmail.com>
2023-06-06 10:55:52 +0000

    Jun 6, 2023, 4:25 PM

--- Express/index.js
+++ Express/index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) => {
   res.send("home page");
+  //   res.sendFile("./index.html");
 });
 app.listen(1000, () => {
   console.log("server executed");
