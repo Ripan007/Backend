557751dd807a349da9e253f3e2bda00e9ae8458d
ripan <reponmondal007@gmail.com>
2023-06-06 05:42:10 +0000

    Jun 6, 2023, 11:12 AM

--- Node/feature.js
+++ Node/feature.js
@@ -1,2 +1,7 @@
+// default type => we can assign any name while accessing the file
 const name = "ripan";
+const name2 = "rajiv";
+const name3 = "mohit";
+
+export { name2, name3 };
 export default name;
