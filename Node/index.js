972883a8ac7275c2793a0e255d4c0adb64e3d8c9
ripan <reponmondal007@gmail.com>
2023-06-06 05:22:35 +0000

    Jun 6, 2023, 10:52 AM

--- Node/index.js
+++ Node/index.js
@@ -5,6 +5,12 @@ const server = http.createServer((req, res) => {
   //   console.log(req.url);
   if (req.url === "/") {
     res.end(" <h1>home page <h1/>  ");
+  } else if (req.url === "/about") {
+    res.end(" <h1> about page <h1/>  ");
+  } else if (req.url === "/contact") {
+    res.end(" <h1> contact page <h1/>  ");
+  } else {
+    res.end(" <h1> no  page found <h1/>  ");
   }
 });
 server.listen(1000, () => {
