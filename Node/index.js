1c3f13f0cf01cd10ed4b71d13750410f9ac16654
ripan <reponmondal007@gmail.com>
2023-06-06 05:37:52 +0000

    Jun 6, 2023, 11:07 AM

--- Node/index.js
+++ Node/index.js
@@ -1,22 +1,23 @@
-// creation  of server , routing
-const http = require("http");
-const server = http.createServer((req, res) => {
-  //   res.end("welcome to the home page");
-  //   console.log(req.url);
-  if (req.url === "/") {
-    res.end(" <h1>home page <h1/>  ");
-  } else if (req.url === "/about") {
-    res.end(" <h1> about page <h1/>  ");
-  } else if (req.url === "/contact") {
-    res.end(" <h1> contact page <h1/>  ");
-  } else {
-    res.end(" <h1> no  page found <h1/>  ");
-  }
-});
-server.listen(1000, () => {
-  console.log("server is working");
-});
-
+// ? creation  of server , routing
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.end("welcome to the home page");
+//   console.log(req.url);
+//   if (req.url === "/") {
+//     res.end(" <h1>home page <h1/>  ");
+//   } else if (req.url === "/about") {
+//     res.end(" <h1> about page <h1/>  ");
+//   } else if (req.url === "/contact") {
+//     res.end(" <h1> contact page <h1/>  ");
+//   } else {
+//     res.end(" <h1> no  page found <h1/>  ");
+//   }
+// });
+// server.listen(1000, () => {
+//   console.log("server is working");
+// });
+const name = require("./feature.js");
+console.log(name);
 // ? terminology to remember
 // server
 // api
