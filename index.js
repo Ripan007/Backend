002505176f3aec66e262f9953c44995fbcaef61f
ripan <reponmondal007@gmail.com>
2023-06-06 03:38:21 +0000

    Jun 6, 2023, 9:08 AM

--- index.js
+++ index.js
@@ -171,6 +171,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("home page");
 });
+app.get("/about", (req, res) => {
+  res.send("about page");
+});
 app.listen(1000, () => {
   console.log("server is running");
 });
