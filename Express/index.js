999245e5e22321c87738b99b3b68fc2d78000eb3
ripan <reponmondal007@gmail.com>
2023-06-06 10:48:36 +0000

    Jun 6, 2023, 4:18 PM

--- Express/index.js
+++ Express/index.js
@@ -1,10 +1,3 @@
-// ? why express ? instead of node js to create server
-// node js framework
-// more  syntactical manner
-// routing is easy
-// split the  code
-// less code
-
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
@@ -13,3 +6,11 @@ app.get("/", (req, res) => {
 app.listen(1000, () => {
   console.log("server executed");
 });
+
+// ? why express ? instead of node js to create server
+// node js framework
+// more  syntactical manner
+// routing is easy
+// split the  code
+// less code
+// javascript tempalating engines
