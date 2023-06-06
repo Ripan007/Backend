1323da0a6d61da97d0c5632c4482d6670c5a3160
ripan <reponmondal007@gmail.com>
2023-06-06 02:52:53 +0000

    Jun 6, 2023, 8:22 AM

--- index.js
+++ index.js
@@ -149,7 +149,7 @@
 // inquirer
 //   .prompt([{ message: "type in your URL: ", name: "URL" }])
 //   .then((answers) => {
-//     // console.log(answers);
+// console.log(answers);
 //     const url = answers.URL;
 //     var qr_svg = qr.image(url);
 //     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
@@ -160,8 +160,8 @@
 //   })
 //   .catch((error) => {
 //     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
+// Prompt couldn't be rendered in the current environment
 //     } else {
-//       // Something else went wrong
+// Something else went wrong
 //     }
 //   });
