0742825133d1a5bf3dbec95c4972267bf4a25c51
ripan <reponmondal007@gmail.com>
2023-06-06 05:16:49 +0000

    Jun 6, 2023, 10:46 AM

--- Node/index.js
+++ Node/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("welcome to the home page");
+  //   res.end("welcome to the home page");
+  //   console.log(req.url);
 });
 server.listen(1000, () => {
   console.log("server is working");
