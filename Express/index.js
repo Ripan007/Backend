b5b8f3d840f38aaa907934fd64fec2acf4c272be
ripan <reponmondal007@gmail.com>
2023-06-07 03:43:24 +0000

    Jun 7, 2023, 9:13 AM

--- Express/index.js
+++ Express/index.js
@@ -1,15 +1,21 @@
 import express from "express";
-import path from "path";
 const app = express();
+/* ------------------- without using ejs (engine template) ------------------ */
+// import path from "path";
 
-app.get("/", (req, res) => {
-  const pathLocation = path.resolve();
-  console.log(pathLocation, "index.html");
-  res.sendFile(path.join(pathLocation, "index.html"));
-});
-app.listen(2000, () => {
-  console.log("server is running");
-});
+// app.get("/", (req, res) => {
+// path.resolve() => we get the   absolute value
+//   const pathLocation = path.resolve();
+//   console.log(pathLocation, "index.html");
+//   res.sendFile(path.join(pathLocation, "index.html"));
+// });
+// app.listen(2000, () => {
+//   console.log("server is running");
+// });
+
+/* -------------------------------- with ejs -------------------------------- */
+app.get("/", (req, res) => {});
+
 // ? why express ? instead of node js to create server
 // node js framework
 // more  syntactical manner
