4ebeef3c07691342bb4dd7f4f22ece8fdc0d9b65
ripan <reponmondal007@gmail.com>
2024-05-12 23:08:48 +0000

    May 13, 2024, 4:38 AM

--- Node/node.js
+++ Node/node.js
@@ -48,6 +48,10 @@ inquirer
     const url = answers.URL
     var qr_svg = qr.image(url)
     qr_svg.pipe(fs.createWriteStream('qr_img.png'))
+    fs.writeFile('URL.txt', url, (err) => {
+      if (err) throw err
+      console.log('the  file has been saved')
+    })
 
     // Use user feedback for... whatever!!
   })
