f3095593e90add5f89a52f4baa31dd195d5964f7
ripan <reponmondal007@gmail.com>
2024-05-12 21:42:24 +0000

    May 13, 2024, 3:12 AM

--- Node/node.js
+++ Node/node.js
@@ -1,7 +1,7 @@
 // node => an asynchronous event-driven javascript runtime ,  node js  is designed to build scalable network applications
 
 const fs = require('fs')
-fs.writeFile('message.txt', (err) => {
+fs.writeFile('message.txt', 'hello node js', (err) => {
   if (err) throw err
   console.log('file is created')
 })
