85c7f222fed39f47fd46fcba8d8913f6e434f00a
ripan <reponmondal007@gmail.com>
2024-05-12 05:59:44 +0000

    May 12, 2024, 11:29 AM

--- Node/node.js
+++ Node/node.js
@@ -5,3 +5,8 @@ fs.writeFile('message.txt', 'work done  with nodejs', (err) => {
   if (err) throw err
   console.log('file has beewn added')
 })
+
+fs.readFile('./message.txt', (err, data) => {
+  if (err) throw err
+  console.log(data)
+})
