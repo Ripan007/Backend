3becd89838c1d771baa1bd974686e927e808ae8d
ripan <reponmondal007@gmail.com>
2024-05-13 03:04:50 +0000

    May 13, 2024, 8:34 AM

--- Express/index.js
+++ Express/index.js
@@ -35,7 +35,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(--dirname + 'public/index.html')
+  res.sendFile(__dirname + 'public/index.html')
 })
 
 app.listen(port, () => {
