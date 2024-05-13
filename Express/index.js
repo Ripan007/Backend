6f024e1a67019170f0a862da296836214d35558e
ripan <reponmondal007@gmail.com>
2024-05-13 00:06:20 +0000

    May 13, 2024, 5:36 AM

--- Express/index.js
+++ Express/index.js
@@ -12,6 +12,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('<h1> home page<h1/>')
 })
+app.get('/contact', (req, res) => {
+  res.send('<h2> contact page<h2/> ')
+})
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
