ade6d7658ff10e87a88bd36ac6e459f3242af386
ripan <reponmondal007@gmail.com>
2024-05-13 11:06:02 +0000

    May 13, 2024, 4:36 PM

---
+++ Ejs/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    dayType: 'a weekday',
+    advice: 'it is time to  work harder',
+  })
+})
+app.listen(port, () => {
+  console.log(`server is running at ${port}`)
+})
