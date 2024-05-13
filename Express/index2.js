2af9414b511966168fa9f93083c6a691198979fc
ripan <reponmondal007@gmail.com>
2024-05-13 09:28:32 +0000

    May 13, 2024, 2:58 PM

--- Express/index2.js
+++ Express/index2.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const app = express()
 const port = 3000
+function logger(req, res, next) {
+  console.log('requested method:', req.method)
+  console.log('requested url:', req.url)
+}
 app.use(logger)
 app.get('/', (req, res) => {
   res.send('hello')
