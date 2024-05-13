cd78d3b18e84617ca7e986f608c1cd4784dff14e
ripan <reponmondal007@gmail.com>
2024-05-13 04:03:22 +0000

    May 13, 2024, 9:33 AM

--- Express/index.js
+++ Express/index.js
@@ -31,13 +31,15 @@ import bodyParser from 'body-parser'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
-
+app.use(bodyParser.url({ extended: true }))
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
+
+app.post('/submit', (req, res) => {})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
