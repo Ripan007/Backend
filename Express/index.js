977688702ff5cba10b5b01f4479254adc48dcdaf
ripan <reponmondal007@gmail.com>
2024-05-13 03:04:09 +0000

    May 13, 2024, 8:34 AM

--- Express/index.js
+++ Express/index.js
@@ -25,3 +25,19 @@ app.listen(port, () => {
 })
 
  **/
+
+import express from 'express'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(--dirname + 'public/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
